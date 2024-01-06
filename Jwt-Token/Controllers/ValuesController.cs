470554111ba920fd72d134f7f6959457856b9e24
oguzhansecgel <oguzhansecgel19@icloud.com>
2024-01-07 00:55:58 +0300

    Add project files.

---
+++ Jwt-Token/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Jwt_Token.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            Token token = TokenHandler.CreateToken(_configuration);
+            return Ok(token);
+        }
+    }
+}
